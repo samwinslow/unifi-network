27884066edc8032d3ed2dab9602a0d892215f8e1
Sam Winslow <4645779+samwinslow@users.noreply.github.com>
2024-07-20 15:03:51 -0400

    update defs

--- fedora-add-systemd.sh
+++ fedora-add-systemd.sh
@@ -1,3 +1,3 @@
 sudo podman-compose systemd -a create-unit
-sudo podman-compose -f unifi.yaml systemd -a register
-sudo systemctl --user enable --now podman-compose@unifi
+podman-compose -f unifi-network.yml systemd -a register
+systemctl --user enable --now podman-compose@unifi-network
